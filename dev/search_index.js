924db2be03396f410959c5647ccb29bb2f28c207
Documenter.jl <documenter@juliadocs.github.io>
2021-02-23 13:31:33 +0000

    build based on e4ed0ad

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MSTM","category":"page"},{"location":"#MSTM","page":"Home","title":"MSTM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MSTM]","category":"page"}]
+}
